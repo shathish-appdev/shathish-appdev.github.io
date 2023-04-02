ec2383815bb69e4f2a33b2bc787e879fb88ab4d3
shathish-appdev <128266671+shathish-appdev@users.noreply.github.com>
2023-04-03 01:08:13 +0530

    Update functions.js

--- pg/functions.js
+++ pg/functions.js
@@ -3,13 +3,13 @@ export function prompt_generator1(input) {
   switch (input) {
     case 'greeting':
       prompt = 'Please provide a new greeting prompt.';
-      break;
+      return prompt;
     case 'action':
       prompt = 'Please provide an new action prompt.';
-      break;
+      return prompt;
     default:
       prompt = 'I want you to become my new Prompt Creator.';
-      break;
+      return prompt;
   }
-  return prompt;
+  
 }
